3451572bc0d99af0f49bdff9299433f59cefd7da
Nigel Small <nasmall@gmail.com>
2013-09-18 14:21:14 +0000

    added blurb

--- server.py
+++ server.py
@@ -16,7 +16,37 @@ XML_GEOFF_TEMPLATE = """\
 <body>
 <form method="POST">
 <h1>XML to Geoff Converter</h1>
-<textarea name="xml" cols="80" rows="24"></textarea>
+<p>This service allows conversion of generic XML data into Geoff, which can then be loaded into Neo4j.</p>
+<h2>Use as a web service</h2>
+To use this API from the command line, use <tt>curl</tt> as below:
+<pre>
+curl -X POST http://api.nigelsmall.com/xml-geoff/ -d @test/files/abba.xml
+</pre>
+<h2>Try on this page</h2>
+<textarea name="xml" cols="80" rows="24" wrap="off"><?xml version="1.0" encoding="UTF-8" ?>
+<group id="abba">
+  <member id="agnetha">
+    <name>Agnetha Fältskog</name>
+    <birth date="1950-04-05" />
+  </member>
+  <member id="björn">
+    <name>Björn Ulvaeus</name>
+    <birth date="1945-04-25" />
+  </member>
+  <member id="benny">
+    <name>Benny Andersson</name>
+    <birth date="1946-12-16" />
+  </member>
+  <member id="frida">
+    <name>Anni-Frid Lyngstad</name>
+    <birth date="1945-11-15" />
+  </member>
+  <song id="waterloo" release_date="1974-03-04">
+    <name>Waterloo</name>
+    <length min="2" sec="42" />
+  </song>
+</group>
+</textarea>
 <br>
 <input type="submit" value="Convert to Geoff">
 </form>
